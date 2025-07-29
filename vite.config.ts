c3676d5f9799e4a2d13ea525f771b16e35c4f112
olaide <holaryidealabi@gmail.comgit config --global user.email holaryidealabi@gmail.com>
2025-07-29 10:10:51 -0700

    add build

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
 });
